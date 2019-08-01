3a5a51e84d5dab3cfd4fef661c46977b091aaa9c
Andrey Kolosov <andrey.kolosov@glidewelldental.com>
2019-08-01 16:58:50 +0300

    Updates

---
+++ precache-manifest.76f0731e41bd518f60c39484fc0291cd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d117a26a1381918543dee5da497cb5a",
+    "url": "/diabloweb/4edcc7263513f350b03e.worker.js"
+  },
+  {
+    "revision": "e7f33f0c305e3ef68e08f009b563aa0c",
+    "url": "/diabloweb/index.html"
+  },
+  {
+    "revision": "1cb86034a63f1e2e5bdd",
+    "url": "/diabloweb/static/css/main.e0aa67f8.chunk.css"
+  },
+  {
+    "revision": "bd27dd3c1db0ac37073f",
+    "url": "/diabloweb/static/js/0.bfbf495f.chunk.js"
+  },
+  {
+    "revision": "9dfc7c12c5cc8cf710dc",
+    "url": "/diabloweb/static/js/5.346cebb4.chunk.js"
+  },
+  {
+    "revision": "1cb86034a63f1e2e5bdd",
+    "url": "/diabloweb/static/js/main.f708879f.chunk.js"
+  },
+  {
+    "revision": "077fe45e700d7971de0b",
+    "url": "/diabloweb/static/js/runtime~main.bf17ce54.js"
+  },
+  {
+    "revision": "4bb3ed3f7b20a9072773",
+    "url": "/diabloweb/static/js/runtime~storage.83a78cef.js"
+  },
+  {
+    "revision": "6fd829b74e6303e98d15",
+    "url": "/diabloweb/static/js/storage.055470d7.chunk.js"
+  },
+  {
+    "revision": "29668cad8644a764aef0edd43154acf5",
+    "url": "/diabloweb/static/media/Diablo.29668cad.wasm"
+  },
+  {
+    "revision": "e8ce827619d27221142883432842f53b",
+    "url": "/diabloweb/static/media/DiabloSpawn.e8ce8276.wasm"
+  },
+  {
+    "revision": "ddc5d817712fdde5a23034ba0b719c78",
+    "url": "/diabloweb/static/media/move.ddc5d817.svg"
+  },
+  {
+    "revision": "bd1e6e6341fd8472679cd9774f8e25b0",
+    "url": "/diabloweb/static/media/rmb.bd1e6e63.svg"
+  },
+  {
+    "revision": "b40e4a63264bd9ddb182072a2fc6b25d",
+    "url": "/diabloweb/static/media/shift.b40e4a63.svg"
+  },
+  {
+    "revision": "8b76aac4599eb594ff5156417595b5fa",
+    "url": "/diabloweb/storage.html"
+  }
+]);
